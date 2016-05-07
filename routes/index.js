4da721b55ba89b8208a6fbc8f23392ee5c2e2285
Niccolò Olivieri <niccolo@olivieriachille.com>
2016-05-07 19:32:01 +0200

    Removed unnecessary view files + fixed some OCD things

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var router = express.Router();
+var router = module.exports = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({
+    result: true
+  });
 });
-
-module.exports = router;
--- routes/upload.js
+++ routes/upload.js
@@ -1,22 +1,20 @@
 'use strict';
 
 var express = require('express');
-var router = express.Router();
+var router = module.exports = express.Router();
 
 /* GET test */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   res.json({
     result: false
   });
 });
 
 /* POST upload a video. */
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
   console.log(req);
   
   res.json({
     result: true
   });
 });
-
-module.exports = router;
