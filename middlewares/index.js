7ce31735bc68b57d56b267e2dcbe7fdb87e3c1ae
Niccolò Olivieri <niccolo@olivieriachille.com>
2016-05-12 09:01:32 +0200

    Fixed the env value to be in development

--- middlewares/index.js
+++ middlewares/index.js
@@ -6,9 +6,9 @@ const bodyParser = require('body-parser');
 
 module.exports = function (app) {
   if (app.get('env') !== 'test') {
-    app.use(logger(app.get('env') === 'dev' ? 'dev' : 'combined'));
+    app.use(logger(app.get('env') === 'development' ? 'dev' : 'combined'));
   }
-  
+
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
