38c4ede5a384ae3b7fe14285d8e9780a8df8fcb3
Niccolò Olivieri <niccolo@olivieriachille.com>
2016-05-16 14:18:00 +0200

    Fixed the strict js directive

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 
 const app = module.exports = express();
--- test/api/user.js
+++ test/api/user.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const common = require('../common');
 
 describe('User', () => {
