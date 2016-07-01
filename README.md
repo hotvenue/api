98f6c19c3684ecd214ba900ceb6bffa67bd7be97
Niccolò Olivieri <niccolo@olivieriachille.com>
2016-07-01 11:19:55 +0200

    Added the virtual attributes for the urls

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## TODO
 
-[ ] mailer (nodemailer + ses transport)
-[ ] pm2 plugins
-[ ] cloud.js -> change credentials with an environment var (web / app)
-[ ] kue
-[ ] application server (different entry point)
+- [x] mailer (nodemailer + ses transport)
+- [ ] pm2 plugins
+- [x] cloud.js -> change credentials with an environment var (web / app)
+- [x] kue
+- [ ] application server (different entry point)
--- app/models/video.js
+++ app/models/video.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const config = require('config');
+
+const configS3 = config.get('aws.s3');
+
 module.exports = function createVideo(sequelize, DataTypes) {
   const video = sequelize.define('video', {
     id: {
@@ -16,6 +20,42 @@ module.exports = function createVideo(sequelize, DataTypes) {
     sent: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
+    },
+
+    urlOriginal: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return [
+          configS3.link,
+          configS3.bucket,
+          configS3.folder.video.original,
+          `${this.getDataValue('id')}.${this.getDataValue('extension')}`,
+        ].join('/');
+      },
+    },
+
+    urlEditedA: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return [
+          configS3.link,
+          configS3.bucket,
+          configS3.folder.video.editedA,
+          `${this.getDataValue('id')}.${this.getDataValue('extension')}`,
+        ].join('/');
+      },
+    },
+
+    urlPreview: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return [
+          configS3.link,
+          configS3.bucket,
+          configS3.folder.video.preview,
+          `${this.getDataValue('id')}.jpg}`,
+        ].join('/');
+      },
     },
   }, {
     classMethods: {
