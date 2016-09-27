18e08c1673611a50e3f9d0e9d0085caf473a94c3
Niccolò Olivieri <niccolo@olivieriachille.com>
2016-09-27 10:59:43 +0200

    Moved the crop filter

--- things/lambda/convertVideo/index.js
+++ things/lambda/convertVideo/index.js
@@ -135,8 +135,7 @@ function doFfmpegA(original, watermark, video) {
     '-i', original,
     '-i', original,
     '-i', original,
-    '-filter_complex', 'concat=n=3:v=1:a=1',
-    '-filter:v', 'crop=in_w:in_w',
+    '-filter_complex', '"crop=in_w:in_w; concat=n=3:v=1:a=1"',
     '-y', tmpVideo1,
   ];
 
