6c790d524d8235d67f24d9501e0a85e5ee92b146
Niccolò Olivieri <niccolo@olivieriachille.com>
2016-10-02 15:34:30 +0200

    Fixed the json problem

--- things/circleci/deploy-server.sh
+++ things/circleci/deploy-server.sh
@@ -6,9 +6,9 @@ JQ="jq --raw-output --exit-status"
 
 CLUSTER_NAME="hotvenue"
 SERVICE_NAME="hotvenue"
-TASK_FAMILY="hotvenue-server"
+FAMILY_NAME="hotvenue-server"
 
-TASK_TEMPLATE='[
+CONTAINER_TEMPLATE='[
   {
     "image": "%s.dkr.ecr.%s.amazonaws.com/%s:%s",
     "name": "%s",
@@ -25,18 +25,16 @@ TASK_TEMPLATE='[
   }
 ]'
 
-TASK_DEFINITION=$(printf "$TASK_TEMPLATE" $TASK_FAMILY $AWS_ACCOUNT_ID $AWS_DEFAULT_REGION $APP_NAME $CIRCLE_BUILD_NUM $SERVICE_NAME)
+CONTAINER_DEFINITION=$(printf "$CONTAINER_TEMPLATE" ${AWS_ACCOUNT_ID} ${AWS_DEFAULT_REGION} ${APP_NAME} ${CIRCLE_BUILD_NUM} ${SERVICE_NAME})
 
-echo ${TASK_DEFINITION}
-
-if REVISION=$(aws ecs register-task-definition --family ${TASK_FAMILY} --container-definitions ${TASK_DEFINITION} | $JQ '.taskDefinition.taskDefinitionArn'); then
+if REVISION=$(aws ecs register-task-definition --family ${FAMILY_NAME} --container-definitions "${CONTAINER_DEFINITION}" | ${JQ} '.taskDefinition.taskDefinitionArn'); then
     echo "Revision: $REVISION"
 else
     echo "Failed to register task definition"
     return 1
 fi
 
-if [[ $(aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${REVISION} | $JQ '.service.taskDefinition') != ${REVISION} ]]; then
+if [[ $(aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${REVISION} | ${JQ} '.service.taskDefinition') != ${REVISION} ]]; then
     echo "Error updating service."
     return 1
 fi
