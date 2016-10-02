80aee0bc433d05b0e6528fd9ba50cf8886edfa7d
Niccolò Olivieri <niccolo@olivieriachille.com>
2016-10-02 14:24:40 +0200

    Added a false test condition

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,14 @@
 'use strict';
 
+const _ = require('lodash');
 const packageObj = require('../../package.json');
 
 module.exports = {
   home(req, res) {
-    res.json(packageObj);
+    res.json(_.pick(packageObj, [
+      'name',
+      'version',
+      'deploy',
+    ]));
   },
 };
